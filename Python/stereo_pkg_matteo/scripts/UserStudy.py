de60d69c0f149868e19b825b69562f1f6421e136
matmag01 <matteo5.magnani@mail.polimi.it>
2025-08-19 16:11:29 +0200

    Change in manifest --> No more error when start project

--- Python/stereo_pkg_matteo/scripts/UserStudy.py
+++ Python/stereo_pkg_matteo/scripts/UserStudy.py
@@ -6,7 +6,7 @@ from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge
 import message_filters
 
-class StereoConcatenator:
+class StereoConcatenatorUserStudy:
     def __init__(self):
         rospy.init_node('stereo_concatenator_sync', anonymous=True)
         self.bridge = CvBridge()
@@ -15,19 +15,19 @@ class StereoConcatenator:
         self.pub = rospy.Publisher('/concatenated_image/compressed', CompressedImage, queue_size=1)
         self.pub_square_left = rospy.Publisher('/left/img_square', Image, queue_size=1)
         self.pub_square_right = rospy.Publisher('/right/img_square', Image, queue_size=1)
-        self.pub_square = rospy.Publisher('/concatenated_image/square_compressed', CompressedImage, queue_size=1)
 
         # Subscribers
         left_sub = message_filters.Subscriber('/dvrk_cam/left/image_raw', Image)
         right_sub = message_filters.Subscriber('/dvrk_cam/right/image_raw', Image)
         ts = message_filters.ApproximateTimeSynchronizer([left_sub, right_sub], queue_size=10, slop=0.05)
         ts.registerCallback(self.callback)
+
         # Overlay user study
         overlay_path = rospy.get_param('~overlay_img', "/home/mmagnan4/Desktop/userstudy")
         self.overlay_static = cv2.imread(overlay_path, cv2.IMREAD_UNCHANGED)
         if self.overlay_static is None:
             rospy.logwarn("No img found")
-
+            return
         
         # Parameters
         self.contrast = rospy.get_param('~contrast', 1.2)
@@ -35,6 +35,7 @@ class StereoConcatenator:
         self.size_factor = rospy.get_param('~size_factor', 0.15) # Square dimension in %
         self.color_str = rospy.get_param('~square_color', "0,255,0") # Square color (default: green)
         self.square_color = tuple(map(int, self.color_str.split(',')))
+        self.quality = 78 #JPEG img quality
 
         rospy.loginfo("StereoConcatenator sync node started with contrast %.2f and brightness %.2f . Square dimension %.2f and color %s", self.contrast, self.brightness, self.size_factor, str(self.square_color))
         rospy.spin()
@@ -96,49 +97,42 @@ class StereoConcatenator:
             rospy.logerr("CvBridge error: %s", e)
             return
 
-        try:
-            concat = np.concatenate((left_cv, right_cv), axis=1)
-            concat = cv2.convertScaleAbs(concat, alpha = self.contrast, beta = self.brightness)
-            msg_out = CompressedImage()
-            msg_out.header.stamp = rospy.Time.now()
-            msg_out.format = "jpeg"
-            msg_out.data = np.array(cv2.imencode('.jpg', concat)[1]).tobytes()
-            self.pub.publish(msg_out)
-        except Exception as e:
-            rospy.logerr("Concatenation or compression error: %s", e)
-
         # Square
-
         try: 
             left_sq = self.add_square(left_cv)
             right_sq = self.add_square(right_cv)
-
-            # Pubblica immagini singole con solo quadrato mobile
+            cv2.imshow("Left with square", left_sq)
+            cv2.imshow("Right with square", right_sq)
+            cv2.waitKey(1)
+            # Publish images
             self.pub_square_left.publish(self.bridge.cv2_to_imgmsg(left_sq, "bgr8"))
             self.pub_square_right.publish(self.bridge.cv2_to_imgmsg(right_sq, "bgr8"))
 
-            # Concatena
+            # Concatenated
             concat_sq = np.concatenate((left_sq, right_sq), axis=1)
-            concat_sq = cv2.convertScaleAbs(concat_sq, alpha=self.contrast, beta=self.brightness)
+            #concat_sq = cv2.convertScaleAbs(concat_sq, alpha=self.contrast, beta=self.brightness)
 
-            # Overlay fisso applicato alla concatenata
+            # JPEG img quality
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), self.quality]
+            result, encimg = cv2.imencode('.jpg', concat_sq, encode_param)
+            # Overlay
             concat_sq_overlay = self.apply_overlay_fixed(concat_sq, self.overlay_static)
 
-            # Pubblica concatenata con overlay fisso
             msg_out_sq = CompressedImage()
             msg_out_sq.header.stamp = rospy.Time.now()
             msg_out_sq.format = "jpeg"
-            msg_out_sq.data = np.array(cv2.imencode('.jpg', concat_sq_overlay)[1]).tobytes()
-            self.pub_square.publish(msg_out_sq)
+            msg_out_sq.data =encimg.tobytes()
+            self.pub.publish(msg_out_sq)
 
         except Exception as e:
             rospy.logerr("Errore concatenazione/compressione img con overlay: %s", e)
+            return
 
 
 
 if __name__ == '__main__':
     try:
-        StereoConcatenator()
+        StereoConcatenatorUserStudy()
     except rospy.ROSInterruptException:
         pass
 
